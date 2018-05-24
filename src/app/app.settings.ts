f4566d1bfebf0a74523b30a83800d279ed463810
shahimsadakath <shahim.sadakath@gmail.com>
2018-05-25 01:16:27 +0300

    Update app.settings.ts

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,8 +1,8 @@
 export class AppSettings {
     
      public static GOODZER_API_ENDPOINT='https://api.goodzer.com/products/v0.1/';
-    public static GOODZER_API_KEY ='422e1b988a756d3713ed2f570c032e39';
-    public static GMAP_URL = "https://maps.google.com/maps/api/js?key=AIzaSyCj7av5uGjKw0mqcKeTCQLAaGlC1SV94e4";
+    public static GOODZER_API_KEY ='{your_API_KEY}';
+    public static GMAP_URL = "https://maps.google.com/maps/api/js?key={your_API_KEY}";
     
  }
  
